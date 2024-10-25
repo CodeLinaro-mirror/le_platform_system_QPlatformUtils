f5fe0471e2dbf34434d2b60b5a608e482baa64ea
Parikshit Thakur <quic_parithak@quicinc.com>
2024-10-25 19:50:17 +0530

    persist-backup: set correct selinux label.
    Change-Id: Idf7b047b9a7b821495db4779e8a12ed77b7044e9

--- hlos-partition-repair/persist-backup.sh
+++ hlos-partition-repair/persist-backup.sh
@@ -5,7 +5,6 @@
 # Take backup of persist partition in persist_bkp partition
 
 
-restorecon -vF /persist_bkp/
 MNT_POINT="/persist"
 
 if mount | grep -q "$MNT_POINT" ;
@@ -16,6 +15,7 @@ then
    if [[ $fsck_status -lt 2 ]]; then
        mount -t ext4 /dev/block/bootdevice/by-name/persist_bkp /persist_bkp -o rootcontext=system_u:object_r:persist_t:s0
        st=$?
+       chcon -t persist_t /persist_bkp
 
        if [[ $st -eq 0 ]]; then
            # Store Persist partition backup in persist_bkp partition
